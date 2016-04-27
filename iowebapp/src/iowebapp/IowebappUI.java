88b29ed19083d0c5bd86d73ce52f4799c198aeb1
Kapitalny <krzysztof.perchlicki@gmail.com>
2016-04-28 01:13:45 +0200

    Istotne poprawki - zgodność z PMD

--- iowebapp/src/iowebapp/IowebappUI.java
+++ iowebapp/src/iowebapp/IowebappUI.java
@@ -11,42 +11,106 @@ import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.themes.ValoTheme;
 
+/**
+ * G³ówny servlet. Po otrzymaniu od u¿ytkownika requestu tworzy UI (User
+ * Interface).
+ * 
+ * @see com.vaadin.ui.UI#init(com.vaadin.server.VaadinRequest)
+ * @author Krzysztof Perchlicki
+ */
 @SuppressWarnings("serial")
 @Theme("iowebapp")
 public class IowebappUI extends UI {
+	
+	/* Wyjaœnienie nazwewnictwa:
+	 * Przyjêta konwencja jest taka, ¿e 1 cz³on oznacza zastosowanie/znaczenie
+	 * zmiennej a 2 cz³on typ obiektu.
+	 * GLay - Grid Layout | DF - DateField
+	 * HLay - Horizontal Layout | VLay - Vertical Layout
+	 * G - Grid | L - Label | TA - TextArea | TF - TextField 
+	 */
 
+	/**
+	 * Bezparametrowy konstruktor z nadklasy (klasa UI).
+	 */
+	public IowebappUI() {
+		super();
+	}
+
+	/**
+	 * Konstruktor z nadklasy (klasa UI) przyjmuj¹cy jako parametr obiekt klasy
+	 * Component.
+	 * 
+	 * @param content
+	 *            referencja do obiektu klasy Component (u¿ywany jako kontent
+	 *            UI).
+	 */
+	public IowebappUI(final Component content) {
+		super(content);
+	}
+
+	/**
+	 * @author Krzysztof Perchlicki
+	 *
+	 */
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = IowebappUI.class)
 	public static class Servlet extends VaadinServlet {
 	}
 
 	/**
-	 * Tworzy GUI u¿ytkownika po otrzymaniu od niego requestu.
-	 *  (non-Javadoc)
+	 * Klasa wewnêtrzna implementuj¹ca interfejs ClickListener dla przycisku
+	 * newEventB. Tworzy obiekt klasy NewEventWindow.
+	 * 
+	 * @author Krzysztof Perchlicki
+	 */
+	// newEventB click listener class
+	class NewEventListener implements ClickListener {
+
+		/**
+		 * Referencja do interfejsu u¿ytkownika (g³ównego servletu). Zostaje
+		 * u¿yta do utworzenia obiektu klasy NewEventWindow
+		 */
+		private final IowebappUI userInterface;
+
+		NewEventListener(final IowebappUI userInterface) {
+			this.userInterface = userInterface;
+		}
+
+		/**
+		 * Nadpisana metoda buttonClick przyjmuj¹ca jako parametr obiekt klasy
+		 * ClickEvent po kliknieciu tworzy obiekt klasy NevEventWindow i
+		 * przekazuje do konstruktora obiekt klasy IowebappUI.
+		 */
+		@Override
+		public void buttonClick(final ClickEvent event) {
+			addWindow(new NewEventWindow(userInterface));
+
+		}
+
+	}
+
+	/**
+	 * Tworzy UI (User Interface) u¿ytkownika po otrzymaniu od niego requestu.
+	 * 
 	 * @see com.vaadin.ui.UI#init(com.vaadin.server.VaadinRequest)
 	 */
 	@Override
-	protected void init(VaadinRequest request) {
+	protected void init(final VaadinRequest request) {
 
 		// mainPanel settings
-		Panel mainPanel = new Panel("EVENT GENERATOR");
-		mainPanel.setWidth("80%");
-		mainPanel.setHeight("43%");
+		final Panel mainPanel = new Panel("EVENT GENERATOR");
+		mainPanel.setWidth(80, Unit.PERCENTAGE);
+		mainPanel.setHeight(43, Unit.PERCENTAGE);
 		setContent(mainPanel);
 
 		// mainHLay settings
-		HorizontalLayout mainHLay = new HorizontalLayout();
+		final HorizontalLayout mainHLay = new HorizontalLayout();
 		mainHLay.setSizeFull();
 		mainPanel.setContent(mainHLay);
 
-		// buttonsGLay settings
-		GridLayout buttonsGLay = new GridLayout(2, 3);
-		mainHLay.addComponent(buttonsGLay);
-		mainHLay.setComponentAlignment(buttonsGLay, Alignment.TOP_LEFT);
-		buttonsGLay.setSizeFull();
-
 		// eventG settings
-		Grid eventG = new Grid();
+		final Grid eventG = new Grid();
 		eventG.addColumn("", String.class);
 		eventG.addColumn("Date start", String.class);
 		eventG.addColumn("Date end", String.class);
@@ -55,43 +119,19 @@ public class IowebappUI extends UI {
 		eventG.setSizeFull();
 		mainHLay.addComponent(eventG);
 
+		// buttonsGLay settings
+		final GridLayout buttonsGLay = new GridLayout(2, 3);
+		mainHLay.addComponent(buttonsGLay);
+		buttonsGLay.setSizeFull();
+
 		// expand and alignment settings of eventG and buttonsGLay
 		mainHLay.setExpandRatio(buttonsGLay, 1);
 		mainHLay.setExpandRatio(eventG, 2);
 		mainHLay.setComponentAlignment(eventG, Alignment.TOP_RIGHT);
 		mainHLay.setComponentAlignment(buttonsGLay, Alignment.TOP_LEFT);
 
-		/**
-		 * Klasa wewnêtrzna implementuj¹ca interfejs ClickListener dla przycisku newEventB.
-		 * Tworzy obiekt klasy NewEventWindow.
-		 * 
-		 * @author Krzysztof Perchlicki
-		 */
-		// newEventB click listener class
-		class NewEventListener implements ClickListener {
-
-			IowebappUI UI;
-
-			NewEventListener(IowebappUI UI) {
-				this.UI = UI;
-			}
-
-			/**
-			 * Nadpisana metoda buttonClick przyjmuj¹ca jako parametr obiekt klasy ClickEvent po kliknieciu 
-			 * tworzy obiekt klasy NevEventWindow i przekazuje do konstruktora obiekt klasy IowebappUI.
-			 * (non-Javadoc)
-			 * @see com.vaadin.ui.Button.ClickListener#buttonClick(com.vaadin.ui.Button.ClickEvent)
-			 */
-			@Override
-			public void buttonClick(ClickEvent event) {
-				addWindow(new NewEventWindow(UI));
-
-			}
-
-		}
-
 		// newEventB settings
-		Button newEventB = new Button("NEW EVENT");
+		final Button newEventB = new Button("NEW EVENT");
 		newEventB.setStyleName(ValoTheme.BUTTON_SMALL, true);
 		newEventB.setImmediate(true);
 		newEventB.setHeight(70, Unit.PERCENTAGE);
@@ -99,35 +139,35 @@ public class IowebappUI extends UI {
 		newEventB.addClickListener(new NewEventListener(this));
 
 		// deleteAllB settings
-		Button deleteAllB = new Button("DELETE ALL");
+		final Button deleteAllB = new Button("DELETE ALL");
 		deleteAllB.setStyleName(ValoTheme.BUTTON_SMALL, true);
 		deleteAllB.setImmediate(true);
 		deleteAllB.setDescription("Click to delete all your events!");
 		deleteAllB.setHeight(70, Unit.PERCENTAGE);
 
 		// loadPcB settings
-		Button loadPcB = new Button("LOAD FROM PC");
+		final Button loadPcB = new Button("LOAD FROM PC");
 		loadPcB.setStyleName(ValoTheme.BUTTON_SMALL, true);
 		loadPcB.setDescription("Click to load your events from your computer!");
 		loadPcB.setImmediate(true);
 		loadPcB.setHeight(70, Unit.PERCENTAGE);
 
 		// loadWebB settings
-		Button loadWebB = new Button("LOAD FROM WEB");
+		final Button loadWebB = new Button("LOAD FROM WEB");
 		loadWebB.setStyleName(ValoTheme.BUTTON_SMALL, true);
 		loadWebB.setDescription("Click to load your events from a website!");
 		loadWebB.setImmediate(true);
 		loadWebB.setHeight(70, Unit.PERCENTAGE);
 
 		// genIcalB settings
-		Button genIcalB = new Button("GENERATE ICAL");
+		final Button genIcalB = new Button("GENERATE ICAL");
 		genIcalB.setStyleName(ValoTheme.BUTTON_SMALL, true);
 		genIcalB.setDescription("Click to export your events to iCal format.");
 		genIcalB.setImmediate(true);
 		genIcalB.setHeight(70, Unit.PERCENTAGE);
 
 		// genCsvB settings
-		Button genCsvB = new Button("GENERATE CSV");
+		final Button genCsvB = new Button("GENERATE CSV");
 		genCsvB.setStyleName(ValoTheme.BUTTON_SMALL, true);
 		genCsvB.setDescription("Click to export your events to CSV format.");
 		genCsvB.setImmediate(true);
--- iowebapp/src/iowebapp/NewEventWindow.java
+++ iowebapp/src/iowebapp/NewEventWindow.java
@@ -13,121 +13,458 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.Window;
 
 /**
- *Klasa NewEventWindow dziedzicz¹ca po klasie Window. Wyœwietla okno w którym u¿ytkownik podaje
- *tytu³, datê startu, datê koñca, lokalizacjê oraz opis wydarzenia.
+ * Klasa NewEventWindow dziedzicz¹ca po klasie Window. Wyœwietla okno w którym
+ * u¿ytkownik podaje tytu³, datê startu, datê koñca, lokalizacjê oraz opis
+ * wydarzenia.
  *
- *@author Krzysztof Perchlicki
+ * @author Krzysztof Perchlicki
  */
 public class NewEventWindow extends Window {
 
 	private static final long serialVersionUID = 1L;
-
-	IowebappUI UI;
-	GridLayout eventGLay;
-	Label titleL, dateStartL, dateEndL, locationL, descriptionL;
-	TextField titleTF, locationTF;
-	TextArea descriptionTA;
-	DateField dateStartDF;
-	DateField dateEndDF;
-	GridLayout mainGLay;
-	Button addB;
+	/**
+	 * Referencja do interfejsu u¿ytkownika (g³ównego servletu).
+	 */
+	private IowebappUI userInterface;
+	/**
+	 * Grid layout na którym znajduj¹ siê pola które nale¿y wype³niæ aby
+	 * utworzyæ wydarzenie.
+	 */
+	private GridLayout eventGLay;
+	/**
+	 * Przycisk który w przypadku gdy odpowiednie pola s¹ wype³nione wysy³a
+	 * informacje do g³ównego servletu (referencja UserInterface).
+	 */
+	private Button addB;
+	/**
+	 * Grid Layout który zawiera w sobie eventGLay i przycisk addB. S³u¿y do
+	 * odpowiedniego pozycjonowania tych komponentów.
+	 */
+	private GridLayout mainGLay;
+	/**
+	 * Wyœwietla napis: "Title".
+	 */
+	private Label titleL;
+	/**
+	 * Wyœwietla napis: "Date start".
+	 */
+	private Label dateStartL;
+	/**
+	 * Wyœwietla napis: "Date end".
+	 */
+	private Label dateEndL;
+	/**
+	 * Wyœwietla napis: "Location".
+	 */
+	private Label locationL;
+	/**
+	 * Wyœwietla napis: "Description"
+	 */
+	private Label descriptionL;
+	/**
+	 * Pola w których podaje siê
+	 */
+	private TextField titleTF, locationTF;
+	/**
+	 * Pole w którym podajê siê opis wydarzenia.
+	 */
+	private TextArea descriptionTA;
+	/**
+	 * Data startu wydarzenia.
+	 */
+	private DateField dateStartDF;
+	/**
+	 * Data koñca wydarzenia.
+	 */
+	private DateField dateEndDF;
 
 	/**
 	 * @param UI
-	 * Konstruktor klasy NewEventWindow, który przyjmujê referencjê do klasy IowebappUI.
+	 *            Konstruktor klasy NewEventWindow,który przyjmujê referencjê do
+	 *            obiektu klasy IowebappUI
 	 */
-	NewEventWindow(IowebappUI UI) {
+	NewEventWindow(final IowebappUI userInterface) {
 		super("New Event");
-		this.UI = UI;
-		
+		this.userInterface = userInterface;
+
 		// mainLayout settings
 		mainGLay = new GridLayout(1, 2);
 		mainGLay.setSizeFull();
-		
+		mainGLay.setRowExpandRatio(0, 9);
+		mainGLay.setRowExpandRatio(1, 1);
+
 		// window settings
 		setWidth("400px");
 		setHeight("500px");
 		center();
 		setResizable(false);
 		setContent(mainGLay);
-		
-		// main layout settings
+
+		// eventGLay settings
 		eventGLay = new GridLayout(2, 5);
 		eventGLay.setSizeFull();
 		eventGLay.setMargin(new MarginInfo(false, true, false, true));
-		
+		mainGLay.addComponent(eventGLay, 0, 0);
+		eventGLay.setColumnExpandRatio(0, 1);
+		eventGLay.setColumnExpandRatio(1, 3);
+
 		// titleL settings
 		titleL = new Label("Title");
 		titleL.setWidth(null);
 		eventGLay.addComponent(titleL, 0, 0);
 		eventGLay.setComponentAlignment(titleL, Alignment.MIDDLE_LEFT);
-		
+
 		// dateStartL settings
 		dateStartL = new Label("Start");
 		dateStartL.setWidth(null);
 		eventGLay.addComponent(dateStartL, 0, 1);
 		eventGLay.setComponentAlignment(dateStartL, Alignment.MIDDLE_LEFT);
-		
+
 		// dateEndL settings
 		dateEndL = new Label("End");
 		dateEndL.setWidth(null);
 		eventGLay.addComponent(dateEndL, 0, 2);
 		eventGLay.setComponentAlignment(dateEndL, Alignment.MIDDLE_LEFT);
-		
+
 		// locationL settings
 		locationL = new Label("Location");
 		locationL.setWidth(null);
 		eventGLay.addComponent(locationL, 0, 3);
 		eventGLay.setComponentAlignment(locationL, Alignment.MIDDLE_LEFT);
-		
+
 		// descriptionL settings
 		descriptionL = new Label("Description");
 		descriptionL.setWidth(null);
 		eventGLay.addComponent(descriptionL, 0, 4);
 		eventGLay.setComponentAlignment(descriptionL, Alignment.MIDDLE_LEFT);
-		
+
 		// titleTF settings
 		titleTF = new TextField();
 		eventGLay.addComponent(titleTF, 1, 0);
 		eventGLay.setComponentAlignment(titleTF, Alignment.MIDDLE_CENTER);
-		
+
 		// dateStartDF settings
 		dateStartDF = new DateField();
 		dateStartDF.setResolution(Resolution.MINUTE);
 		dateStartDF.setLocale(new Locale("en", "GB"));
-		dateStartDF.setWidth("186px");
+		dateStartDF.setWidth(186, Unit.PIXELS);
 		eventGLay.addComponent(dateStartDF, 1, 1);
 		eventGLay.setComponentAlignment(dateStartDF, Alignment.MIDDLE_CENTER);
-		
+
 		// dateEndDF settings
 		dateEndDF = new DateField();
 		dateEndDF.setResolution(Resolution.MINUTE);
 		dateEndDF.setLocale(new Locale("en", "GB"));
-		dateEndDF.setWidth("186px");
+		dateEndDF.setWidth(186, Unit.PIXELS);
 		eventGLay.addComponent(dateEndDF, 1, 2);
 		eventGLay.setComponentAlignment(dateEndDF, Alignment.MIDDLE_CENTER);
-		
+
 		// locationTF settings
 		locationTF = new TextField();
 		eventGLay.addComponent(locationTF, 1, 3);
 		eventGLay.setComponentAlignment(locationTF, Alignment.MIDDLE_CENTER);
-		
+
 		// descriptionTA settings
 		descriptionTA = new TextArea();
 		eventGLay.addComponent(descriptionTA, 1, 4);
 		eventGLay.setComponentAlignment(descriptionTA, Alignment.MIDDLE_CENTER);
-		mainGLay.addComponent(eventGLay, 0, 0);
-		eventGLay.setColumnExpandRatio(0, 1);
-		eventGLay.setColumnExpandRatio(1, 3);
-		
+
 		// addButt settings
 		addB = new Button("Add event");
 		mainGLay.addComponent(addB, 0, 1);
 		mainGLay.setComponentAlignment(addB, Alignment.MIDDLE_CENTER);
-		
-		// expand of rows settings
-		mainGLay.setRowExpandRatio(0, 9);
-		mainGLay.setRowExpandRatio(1, 1);
 	}
 
+	/**
+	 * Metoda, która dajê dostêp do pola userInterface
+	 * 
+	 * @return referencja do userInterface
+	 */
+	public IowebappUI getUserInterface() {
+		return userInterface;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ userInterface.
+	 * 
+	 * @param userInterface
+	 *            referencja do UI (User Interface).
+	 * 
+	 */
+	public void setUserInterface(final IowebappUI userInterface) {
+		this.userInterface = userInterface;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola eventGLay
+	 * 
+	 * @return referencja do eventGLay
+	 */
+	public GridLayout getEventGLay() {
+		return eventGLay;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ eventGLay
+	 * 
+	 * @param eventGLay
+	 *            referencja do obiektu klasy Grid Layout (layout zdarzeñ).
+	 */
+
+	public void setEventGLay(final GridLayout eventGLay) {
+		this.eventGLay = eventGLay;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola addB
+	 * 
+	 * @return referencja do addB
+	 */
+
+	public Button getAddB() {
+		return addB;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ addB
+	 * 
+	 * @param addB
+	 *            referencja do obiektu klasy Button (przycisk dodawania
+	 *            zdarzenia).
+	 */
+
+	public void setAddB(final Button addB) {
+		this.addB = addB;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola mainGLay
+	 * 
+	 * @return referencja do mainGLay
+	 */
+
+	public GridLayout getMainGLay() {
+		return mainGLay;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ mainGLay
+	 * 
+	 * @param mainGLay
+	 *            referencja do obiektu klasy Grid Layout (g³ówny layout).
+	 */
+
+	public void setMainGLay(final GridLayout mainGLay) {
+		this.mainGLay = mainGLay;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola titleL
+	 * 
+	 * @return referencja do titleL
+	 */
+	public Label getTitleL() {
+		return titleL;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ titleL
+	 * 
+	 * @param titleL
+	 *            referencja do obiektu klasy Label (tytu³).
+	 */
+
+	public void setTitleL(final Label titleL) {
+		this.titleL = titleL;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola dateStartL
+	 * 
+	 * @return referencja do dateStartL
+	 */
+
+	public Label getDateStartL() {
+		return dateStartL;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ dateStartL
+	 * 
+	 * @param dateStartL
+	 *            referencja do obiektu klasy Label (data startu).
+	 */
+
+	public void setDateStartL(final Label dateStartL) {
+		this.dateStartL = dateStartL;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola dateEndL
+	 * 
+	 * @return referencja do dateEndL
+	 */
+
+	public Label getDateEndL() {
+		return dateEndL;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ dateEndL
+	 * 
+	 * @param dateEndL
+	 *            referencja do obiektu klasy Label (data koñca).
+	 */
+
+	public void setDateEndL(final Label dateEndL) {
+		this.dateEndL = dateEndL;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola locationL
+	 * 
+	 * @return referencja do locationL
+	 */
+
+	public Label getLocationL() {
+		return locationL;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ locationL
+	 * 
+	 * @param locationL
+	 *            referencja do obiektu klasy Label (lokalizacja zdarzenia).
+	 */
+
+	public void setLocationL(final Label locationL) {
+		this.locationL = locationL;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola descriptionL
+	 * 
+	 * @return referencja do descriptionL
+	 */
+
+	public Label getDescriptionL() {
+		return descriptionL;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ descriptionL
+	 * 
+	 * @param descriptionL
+	 *            referencja do obiektu klasy Label (opis zdarzenia).
+	 */
+
+	public void setDescriptionL(final Label descriptionL) {
+		this.descriptionL = descriptionL;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola titleTF
+	 * 
+	 * @return referencja do titleTF
+	 */
+
+	public TextField getTitleTF() {
+		return titleTF;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ titleTF
+	 * 
+	 * @param titleTF
+	 *            referencja do obiektu klasy TextField (pole tytu³u zdarzenia).
+	 */
+
+	public void setTitleTF(final TextField titleTF) {
+		this.titleTF = titleTF;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola locationTF
+	 * 
+	 * @return referencja do locationTF
+	 */
+
+	public TextField getLocationTF() {
+		return locationTF;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ locationTF
+	 * 
+	 * @param locationTF
+	 *            referencja do obiektu klasy TextField (pole lokalizacji
+	 *            zdarzenia).
+	 */
+
+	public void setLocationTF(final TextField locationTF) {
+		this.locationTF = locationTF;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola descriptionTA
+	 * 
+	 * @return referencja do descriptionTA
+	 */
+
+	public TextArea getDescriptionTA() {
+		return descriptionTA;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ descriptionTA
+	 * 
+	 * @param descriptionTA
+	 *            referencja do obiektu klasy TextArea (pole opisu zdarzenia).
+	 */
+
+	public void setDescriptionTA(final TextArea descriptionTA) {
+		this.descriptionTA = descriptionTA;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola dateStartDF
+	 * 
+	 * @return referencja do dateStartDF
+	 */
+
+	public DateField getDateStartDF() {
+		return dateStartDF;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ dateStartDF
+	 * 
+	 * @param dateStartDF
+	 *            referencja do obiektu klasy DataField (data startu zdarzenia).
+	 */
+
+	public void setDateStartDF(final DateField dateStartDF) {
+		this.dateStartDF = dateStartDF;
+	}
+
+	/**
+	 * Metoda, która dajê dostêp do pola dateEndDF
+	 * 
+	 * @return referencja do dateEndDF
+	 */
+
+	public DateField getDateEndDF() {
+		return dateEndDF;
+	}
+
+	/**
+	 * Metoda, która pozwala zmieniæ dateEndDF
+	 * 
+	 * @param dateEndDF
+	 *            referencja do obiektu klasy DataField (data koñca zdarzenia).
+	 */
+
+	public void setDateEndDF(final DateField dateEndDF) {
+		this.dateEndDF = dateEndDF;
+	}
 }
