8bed5007f65fd3d4e09f1f552169c953785491e9
Adrian Bizukojc <a.bizukojc@interia.pl>
2016-06-14 17:38:11 +0200

    Zaktualizowany plik AllTests.java

--- iowebapp/test/testiowebapp/AllTests.java
+++ iowebapp/test/testiowebapp/AllTests.java
@@ -5,8 +5,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BooleanToFontIconConverterTest.class, CsvGeneratorTest.class, IcalGeneratorTest.class,
-		LoadFromWebWindowTest.class, NewEventWindowTest.class })
+@SuiteClasses({ BooleanToFontIconConverterTest.class, CalendarEventTest.class, CsvGeneratorTest.class,
+		DeleteAllWindowTest.class, IcalGeneratorTest.class, IowebappUITest.class, LoadFromWebWindowTest.class,
+		NewEventWindowTest.class, ParsUZTest.class, UploadWindowTest.class })
 public class AllTests {
 
 }
